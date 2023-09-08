86d4e07d1e9585777dc735492893ce9b18098416
Alexis <alkaloge@cern.ch>
2023-09-08 18:11:44 -0500

    latest

--- Paper/MET/condor_jdls/merge.py
+++ Paper/MET/condor_jdls/merge.py
@@ -4,17 +4,22 @@ import sys
 years = ["2018", "2017", "2016", "2016pre"]
 years = ["2016" , "2016preVFP"]
 years = ["2017", "2018", "2016preVFP" ]
+years = ["2016preVFP"]
 years = ["2018"]
 channels = ["MuMu" ,"ElEl"]
 channels = ["MuMu"]
 channels = ["Gjets"]
 Njet=['eq0', 'eq1', 'geq1', 'incl']
 if 'Gjets' in channels : Njet=['eq1', 'geq1', 'incl']
-if 'Gjets' in channels : Njet=['eq1', 'incl']
-if 'Gjets' in channels : Njet=['incl']
+#if 'Gjets' in channels : Njet=['eq1', 'incl']
+#if 'Gjets' in channels : Njet=['incl']
 #MC = ['QCD_HT1000to1500MG',    'QCD_HT100to200MG' ,     'QCD_HT1500to2000MG',    'QCD_HT2000toInfMG' ,    'QCD_HT200to300MG',    'QCD_HT300to500MG' ,    'QCD_HT500to700MG' ,    'QCD_HT50to100MG' ,    'QCD_HT700to1000MG' 'WJetsToLNu_NLO' ]
 
 MC = ['GJets_HT-40To100', 'GJets_HT-100To200', 'GJets_HT-200To400','GJets_HT-400To600','GJets_HT-600ToInf', 'QCD_HT1000to1500MG',    'QCD_HT100to200MG' ,     'QCD_HT1500to2000MG',    'QCD_HT2000toInfMG' ,    'QCD_HT200to300MG',        'QCD_HT300to500MG' ,    'QCD_HT500to700MG' ,    'QCD_HT50to100MG' ,    'QCD_HT700to1000MG' , 'WJetsToLNu_NLO']
+#MC = ['GJets_HT-40To100', 'GJets_HT-100To200', 'GJets_HT-200To400','GJets_HT-400To600','GJets_HT-600ToInf']
+#MC = ['QCD_HT1000to1500MG',    'QCD_HT100to200MG' ,     'QCD_HT1500to2000MG',    'QCD_HT2000toInfMG' ,    'QCD_HT200to300MG',        'QCD_HT300to500MG' ,    'QCD_HT500to700MG' ,    'QCD_HT50to100MG' ,    'QCD_HT700to1000MG' , 'WJetsToLNu_NLO']
+
+#MC = ['QCD_HT100to200MG']
 
 command = sys.argv[1]
 if command.lower() == "merge" or command.lower()=='step1': 
@@ -23,10 +28,11 @@ if command.lower() == "merge" or command.lower()=='step1':
 	for channel in channels:
             for njet in Njet:
 		if channel == "Gjets" :
-                    #if '2018' in year: os.system("hadd -f scales_EGamma_Run{0}_{1}_Njet{2}.root {1}_{0}/scales_Njet{2}_all_EGamma_Run{0}*root".format(year, channel, njet))
-                    if '2018' not in year: os.system("hadd -f scales_SinglePhoton_Run{0}_{1}_Njet{2}.root {1}_{0}/scales_Njet{2}_all_SinglePhoton_Run{0}*root".format(year, channel, njet))
+                    if '2018' in year: os.system("hadd -f scales_EGamma_Run{0}_{1}_Njet{2}.root {1}_{0}/scales_Njet{2}_all_EGamma_Run{0}*root".format(year, channel, njet))
+                    if '2018' not in year and 'preV' not in year : os.system("hadd -f scales_SinglePhoton_Run{0}_{1}_Njet{2}.root {1}_{0}/scales_Njet{2}_all_SinglePhoton_Run{0}*root".format(year, channel, njet))
+                    if 'preV' in year : os.system("hadd -f scales_SinglePhoton_Run{0}_{1}_Njet{2}.root {1}_{0}/scales_Njet{2}_all_SinglePhoton_Run*root".format(year, channel, njet))
                     for mc in MC : 
-                        #os.system("hadd -f scales_{3}_{0}_{1}_Njet{2}.root {1}_{0}/scales_Njet{2}_{3}_*{1}.root".format(year, channel, njet,mc))
+                        os.system("hadd -f scales_{3}_{0}_{1}_Njet{2}.root {1}_{0}/scales_Njet{2}_{3}_*{1}.root".format(year, channel, njet,mc))
                         os.system("python merge_histos.py {0} {1} {2}".format(mc, year, njet))
 
 
@@ -67,9 +73,12 @@ if command.lower() == "subtract" or command.lower() == 'step2':
 
 		if channel == "Gjets" : 
 		    if year == "2018" :
-                        os.system("hadd -f weighted_scales_EGamma_Run{0}_{1}_Njet{2}.root scales_EGamma_Run{0}_{1}_Njet{2}.root weighted_mc_scales_QCD*_{0:s}_{1}_Njet{2}.root weighted_mc_scales_WJetsToLNu_NLO*_{0:s}_{1}_Njet{2}.root".format(year, channel, njet))
+                        print " weighted_scales_EGamma_Run{0}_{1}_Njet{2}.root scales_EGamma_Run{0}_{1}_Njet{2}.root weighted_mc_scales_QCD_HT*_{0:s}_{1}_Njet{2}.root weighted_mc_scales_WJetsToLNu_NLO*_{0:s}_{1}_Njet{2}.root".format(year, channel, njet)
+                        #os.system("hadd -f weighted_scales_EGamma_Run{0}_{1}_Njet{2}.root scales_EGamma_Run{0}_{1}_Njet{2}.root weighted_mc_scales_QCD_HT*_{0:s}_{1}_Njet{2}.root weighted_mc_scales_WJetsToLNu_NLO*_{0:s}_{1}_Njet{2}.root".format(year, channel, njet))
+                        os.system("hadd -f weighted_scales_EGamma_Run{0}_{1}_Njet{2}.root scales_EGamma_Run{0}_{1}_Njet{2}.root weighted_mc_scales_QCD_HT*_{0:s}_{1}_Njet{2}.root".format(year, channel, njet))
                     if year != "2018" :
-                        os.system("hadd -f weighted_scales_SinglePhoton_Run{0}_{1}_Njet{2}.root scales_SinglePhoton_Run{0}_{1}_Njet{2}.root weighted_mc_scales_QCD*_{0:s}_{1}_Njet{2}.root weighted_mc_scales_WJetsToLNu_NLO*_{0:s}_{1}_Njet{2}.root".format(year, channel, njet))
+                        #os.system("hadd -f weighted_scales_SinglePhoton_Run{0}_{1}_Njet{2}.root scales_SinglePhoton_Run{0}_{1}_Njet{2}.root weighted_mc_scales_QCD_HT*_{0:s}_{1}_Njet{2}.root weighted_mc_scales_WJetsToLNu_NLO*_{0:s}_{1}_Njet{2}.root".format(year, channel, njet))
+                        os.system("hadd -f weighted_scales_SinglePhoton_Run{0}_{1}_Njet{2}.root scales_SinglePhoton_Run{0}_{1}_Njet{2}.root weighted_mc_scales_QCD_HT*_{0:s}_{1}_Njet{2}.root".format(year, channel, njet))
                     os.system("hadd -f weighted_scales_GJetsHT{0}_{1}_Njet{2}.root weighted_mc_scales_GJets_HT-*_{0:s}_{1}_Njet{2}.root ".format(year, channel, njet))
 
 
